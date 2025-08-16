9d285f15ae3f7e0661d3222e0d8c75d6c91ade62
Shtekata <gesheval@gmail.com>
2025-08-16 19:33:45 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// console.log('TAILWIND CONFIG IS LOADED')
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+  },
+}
