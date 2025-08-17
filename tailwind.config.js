7c25207083e518b8cd55102ae01eff72c743a516
Shtekata <gesheval@gmail.com>
2025-08-17 21:43:38 +0300

    Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ export default {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      sm: '480px',
+      sm: '640px',
       md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      spacing: {
+        180: 'var(--spacing-180)',
+        200: 'var(--spacing-200)',
+        220: 'var(--spacing-220)',
+        230: 'var(--spacing-230)',
+      },
     },
   },
 }
